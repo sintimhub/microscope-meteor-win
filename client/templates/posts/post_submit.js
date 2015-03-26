baaf008f861fdbac3e932eaa54a8a04dc4480bf6
Sintim Nuamah <sintim.nuamah@gmail.com>
2015-03-26 12:44:03 -0700

    Enforce post URL uniqueness.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,6 +9,9 @@ Template.postSubmit.events({
 		Meteor.call ('postInsert',post,function(error,result) {
 			if (error)
 				return alert(errror.reason);
+			if (result.postExists)
+				alert('This link has already been posted');
+			
 			Router.go('postPage', {_id: result._id});
 		});
 	}
