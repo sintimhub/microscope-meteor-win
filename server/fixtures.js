f52eb410a45fdb602b9452336070028716fbc420
Sintim Nuamah <sintim.nuamah@gmail.com>
2015-04-10 02:30:47 -0700

    Added basic upvoting algorithm.

--- server/fixtures.js
+++ server/fixtures.js
@@ -16,7 +16,9 @@ if (Posts.find().count() === 0) {
 		author: tom.profile.name,
 		url: 'http://sachagreif.com/Introducing-telescope/',
 		submitted: new Date(now - 7 * 3600 * 1000),
-		commentsCount: 2
+		commentsCount: 2,
+		upvoters: [],
+		votes: 0
 	});
 
 	Comments.insert({
@@ -41,7 +43,9 @@ if (Posts.find().count() === 0) {
 		author: tom.profile.name,
 		url: 'http://meteor.com',
 		submitted: new Date(now - 10 * 3600 * 1000),
-		commentsCount: 0
+		commentsCount: 0,
+		upvoters: [],
+		votes: 0
 	});
 
 	Posts.insert({
@@ -50,7 +54,9 @@ if (Posts.find().count() === 0) {
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
 		submitted:new Date(now - 12 * 3600 * 1000),
-		commentsCount: 0
+		commentsCount: 0,
+		upvoters: [],
+		votes: 0
 	});
 
 	for (var i = 0; i < 20; i++) {
@@ -59,8 +65,10 @@ if (Posts.find().count() === 0) {
 			author: sacha.profile.name,
 			userId: sacha._id,
 			url: 'http://google.com/?q=test-' + i,
-			submitted:new Date(now - i * 3600 * 1000),
-			commentsCount: 0
+			submitted:new Date(now - i * 3600 * 1000 + 1),
+			commentsCount: 0,
+			upvoters: [],
+		votes: 0
 		});
 	}
 }
